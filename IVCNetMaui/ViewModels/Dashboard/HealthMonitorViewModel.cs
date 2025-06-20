d5eae2c02313d435a21aa39ac0a1c2a8bf69da1f
Nam Nguyen <145630409+namhnguyen0103@users.noreply.github.com>
2025-06-20 16:14:53 -0400

    add datagrid

--- IVCNetMaui/ViewModels/Dashboard/HealthMonitorViewModel.cs
+++ IVCNetMaui/ViewModels/Dashboard/HealthMonitorViewModel.cs
@@ -1,18 +1,40 @@
-﻿using IVCNetMaui.Services.Navigation;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using IVCNetMaui.Services.Navigation;
 using IVCNetMaui.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace IVCNetMaui.ViewModels.Dashboard
 {
-    public class HealthMonitorViewModel : ViewModelBase
+    public partial class HealthMonitorViewModel : ViewModelBase
     {
+        [ObservableProperty]
+        public ObservableCollection<Network> networks = new ObservableCollection<Network>
+        {
+            new() {
+                Name = "eth0",
+                NIT = "6",
+                Send = "10.54 Kbps",
+                Receive = "23.76 Kbps",
+                PktQd = "0"
+            }
+        };
         public HealthMonitorViewModel(INavigationService navigationService) : base(navigationService)
         {
 
+        }
+
+        public class Network
+        {
+            public string Name { get; set; }
+            public string NIT { get; set; }
+            public string Send { get; set; }
+            public string Receive { get; set; }
+            public string PktQd { get; set; }
         }
     }
 }
