f0c626f3f167aed8686f76a96734645e121b87c1
Nam Nguyen <145630409+namhnguyen0103@users.noreply.github.com>
2025-06-24 14:40:55 -0400

    implement iot accordion tab

--- IVCNetMaui/ViewModels/Detail/EdgeDetailViewModel.cs
+++ IVCNetMaui/ViewModels/Detail/EdgeDetailViewModel.cs
@@ -1,9 +1,11 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using IVCNetMaui.Controls;
 using IVCNetMaui.Services.Navigation;
 using IVCNetMaui.ViewModels.Base;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,12 +13,16 @@ using System.Windows.Input;
 
 namespace IVCNetMaui.ViewModels.Detail
 {
-    public class EdgeDetailViewModel : ViewModelBase
+    public partial class EdgeDetailViewModel : ViewModelBase
     {
         public ICommand NavigateToHealthMonitorCommand { get; private set; }
+        [ObservableProperty]
+        public ObservableCollection<String> cameras = new();
         public EdgeDetailViewModel(INavigationService navigationService) : base(navigationService) 
         {
             NavigateToHealthMonitorCommand = new AsyncRelayCommand(() => NavigationService.NavigateToAsync("healthMonitor"));
+            Cameras.Add("Item 1");
+            Cameras.Add("Item 2");
         }
     }
 }
