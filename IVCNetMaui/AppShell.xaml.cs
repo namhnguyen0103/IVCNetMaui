aa984c99f35c82c43f1d7945b1fa8559ece33255
Nam Nguyen <namhnguyen0103@gmail.com>
2025-07-07 10:08:17 -0400

    implement role-based access

--- IVCNetMaui/AppShell.xaml.cs
+++ IVCNetMaui/AppShell.xaml.cs
@@ -1,7 +1,8 @@
-﻿using IVCNetMaui.Services.Navigation;
+﻿using IVCNetMaui.Models;
 using IVCNetMaui.Views;
 using IVCNetMaui.Views.Dashboard;
 using IVCNetMaui.Views.Historical;
+using IVCNetMaui.Views.Detail;
 using IVCNetMaui.Views.View;
 using UraniumUI.Icons.MaterialSymbols;
 
@@ -10,129 +11,133 @@ namespace IVCNetMaui
     public partial class AppShell : Shell
     {
         private readonly IServiceProvider _serviceProvider;
-        public AppShell(IServiceProvider serviceProvider)
+        private readonly GlobalSetting _globalSetting;
+        public AppShell(IServiceProvider serviceProvider,  GlobalSetting globalSetting)
         {
             _serviceProvider = serviceProvider;
+            _globalSetting = globalSetting;
 
-            var dashboard = new FlyoutItem
-            {
-                Title = "Dashboard",
-                Route = "dashboard",
-                FlyoutIcon = GetIcon(MaterialSharp.Dashboard),
-                Items =
-                {
-                    new ShellContent
-                    {
-                        Title = "Hub",
-                        ContentTemplate = new DataTemplate(typeof(DashboardMainPage))
-                    }
-                }
-            };
-
-            var cameraView = new FlyoutItem
-            {
-                Title = "Camera View",
-                Route = "cameraView",
-                FlyoutIcon = GetIcon(MaterialSharp.Videocam),
-                Items =
-                {
-                    new ShellContent
-                    {
-                        Title = "Camera View",
-                        ContentTemplate = new DataTemplate(typeof(CameraPage))
-                    }
-                }
-            };
-            
-            var eventView = new FlyoutItem
-            {
-                Title = "Event View",
-                Route = "eventView",
-                FlyoutIcon = GetIcon(MaterialSharp.Flag),
-                Items =
-                {
-                    new ShellContent
-                    {
-                        Title = "Event View",
-                        ContentTemplate = new DataTemplate(typeof(EventPage))
-                    }
-                }
-            };
-
-            var historicalTab = new Tab
-            {
-                FlyoutIcon = GetIcon(MaterialSharp.History),
-                Items = 
-                {
-                    new ShellContent
-                    {
-                        Title = "Health Data",
-                        Route = "healthData",
-                        ContentTemplate = new DataTemplate(typeof(HistoricalHealthDataPage))
-                    },
-                    new ShellContent
-                    {
-                        Title = "IoT Data",
-                        Route = "iotData",
-                        ContentTemplate = new DataTemplate(typeof(IoTDataPage))
-                    },
-                    new ShellContent
-                    {
-                        Title = "Media Data",
-                        Route = "mediaData",
-                        ContentTemplate = new DataTemplate(typeof(MediaDataPage))
-                    }
-                }
-            };
-
-            var historical = new FlyoutItem
-            {
-                Title = "Historical",
-                Route = "historical",
-                FlyoutIcon = GetIcon(MaterialSharp.History),
-                Items =
-                {
-                    historicalTab,
-                }
-            };
-    
-            Items.Add(dashboard);
-            Items.Add(cameraView);
-            Items.Add(eventView);
-            Items.Add(historical);
-            
-            AppShell.InitializeRouting();
-            
+            InitializeAccess();
             InitializeComponent();
         }
 
-        private static void InitializeRouting() 
-        { 
-            Routing.RegisterRoute("dashboard/healthMonitor", typeof(Views.Dashboard.HealthMonitorPage));
-            Routing.RegisterRoute("dashboard/edgeDetail", typeof(Views.Detail.EdgeDetailPage));
-            Routing.RegisterRoute("dashboard/edgeDetail/healthMonitor", typeof(HealthMonitorPage));
-            Routing.RegisterRoute("eventView/eventDetail", typeof(Views.Detail.EventDetailPage));
-            Routing.RegisterRoute("eventView/eventDetail/mediaDetail", typeof(Views.Detail.MediaDetailPage));
-        }
-
-        private void LogoutMenuItem_Clicked(object sender, EventArgs e)
+        private async void LogoutMenuItem_Clicked(object sender, EventArgs e)
         {
+            await _globalSetting.ResetGlobalSettingAsync();
             Application.Current.MainPage = _serviceProvider.GetRequiredService<LoginPage>();
             // Add logic for logout
         }
 
-        private void AssignAccess()
+        private void InitializeAccess()
         {
-            
+            // var webPortalFlag = _globalSetting.Roles[0].WebPortalPermFlags;
+            var webPortalFlag = 5;
+
+            // Allow access to Navigation Section
+            if ((webPortalFlag & _globalSetting.Flags[100]) != 0)
+            {
+                Items.Add(new FlyoutItem
+                {
+                    Title = "Dashboard",
+                    Route = "dashboard",
+                    FlyoutIcon = GetIcon(MaterialSharp.Dashboard),
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Title = "Hub",
+                            ContentTemplate = new DataTemplate(typeof(DashboardMainPage))
+                        }
+                    }
+                });
+                Routing.RegisterRoute("dashboard/healthMonitor", typeof(HealthMonitorPage));
+                Routing.RegisterRoute("dashboard/edgeDetail", typeof(EdgeDetailPage));
+                Routing.RegisterRoute("dashboard/edgeDetail/healthMonitor", typeof(HealthMonitorPage));
+            }
+
+            // Allow access to Views Section
+            if ((webPortalFlag & _globalSetting.Flags[101]) != 0)
+            {
+                Items.Add(new FlyoutItem
+                {
+                    Title = "Camera View",
+                    Route = "cameraView",
+                    FlyoutIcon = GetIcon(MaterialSharp.Videocam),
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Title = "Camera View",
+                            ContentTemplate = new DataTemplate(typeof(CameraPage))
+                        }
+                    }
+                });
+                Items.Add(new FlyoutItem
+                {
+                    Title = "Event View",
+                    Route = "eventView",
+                    FlyoutIcon = GetIcon(MaterialSharp.Flag),
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Title = "Event View",
+                            ContentTemplate = new DataTemplate(typeof(EventPage))
+                        }
+                    }
+                });
+                Routing.RegisterRoute("eventView/eventDetail", typeof(EventDetailPage));
+                Routing.RegisterRoute("eventView/eventDetail/mediaDetail", typeof(MediaDetailPage));
+            }
+
+            // Allow access to Historical Section
+            if ((webPortalFlag & _globalSetting.Flags[102]) != 0)
+            {
+                var historicalTab = new Tab
+                {
+                    FlyoutIcon = GetIcon(MaterialSharp.History),
+                    Items =
+                    {
+                        new ShellContent
+                        {
+                            Title = "Health Data",
+                            Route = "healthData",
+                            ContentTemplate = new DataTemplate(typeof(HistoricalHealthDataPage))
+                        },
+                        new ShellContent
+                        {
+                            Title = "IoT Data",
+                            Route = "iotData",
+                            ContentTemplate = new DataTemplate(typeof(IoTDataPage))
+                        },
+                        new ShellContent
+                        {
+                            Title = "Media Data",
+                            Route = "mediaData",
+                            ContentTemplate = new DataTemplate(typeof(MediaDataPage))
+                        }
+                    }
+                };
+                Items.Add(new FlyoutItem
+                {
+                    Title = "Historical",
+                    Route = "historical",
+                    FlyoutIcon = GetIcon(MaterialSharp.History),
+                    Items =
+                    {
+                        historicalTab,
+                    }
+                });
+            }
         }
         
-        private FontImageSource GetIcon(string iconName)
+        private static FontImageSource GetIcon(string iconName)
         {
             return new FontImageSource
             {
                 FontFamily = "MaterialSharp",
                 Glyph = iconName,
-                Color = Application.Current.Resources["Gray700"] as Color
+                Color = Application.Current?.Resources["Gray700"] as Color ?? Color.FromRgb(255, 255, 255)
             };
         }
     }
--- IVCNetMaui/GlobalSetting.cs
+++ IVCNetMaui/GlobalSetting.cs
@@ -4,8 +4,26 @@ namespace IVCNetMaui;
 
 public class GlobalSetting
 {
+    public Task ResetGlobalSettingAsync()
+    {
+        BaseApiEndpoint = string.Empty;
+        Permissions = new List<Permission>();
+        Roles = new List<Role>();
+        Units = new List<Unit>();
+        Flags = new()
+        {
+            {100, 0},
+            {101, 0},
+            {102, 0},
+            {103, 0},
+            {104, 0},
+            {105, 0},
+            {106, 0},
+        };
+        return Task.CompletedTask;
+    }
+    
     public string BaseApiEndpoint { get; set; } = string.Empty;
-
     private List<Permission> _permissions =  new();
     public List<Permission> Permissions
     {
@@ -13,9 +31,24 @@ public class GlobalSetting
         set
         {
             _permissions = value.Where(perm => perm.PermType == 3).ToList();
+            foreach (var perm in _permissions)
+            {
+                Flags[perm.Id] = perm.Bitmask;
+            }
         }
     }
     
     public List<Role> Roles { get; set; } = new List<Role>();
     public List<Unit> Units { get; set; } = new List<Unit>();
+
+    public Dictionary<int, int> Flags = new()
+    {
+        {100, 0},
+        {101, 0},
+        {102, 0},
+        {103, 0},
+        {104, 0},
+        {105, 0},
+        {106, 0},
+    };
 }
